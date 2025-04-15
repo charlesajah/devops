23480ed0baea91f407a1bc5d9d5d6c901c723653
CHARLES  AJAH <36276306+charlesajah@users.noreply.github.com>
2025-04-15 16:52:19 +0100

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/charlesajah/devops.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('charles.ajah@gmail.com')
         }
