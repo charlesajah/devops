c633315457d6f52466148d1d741882bd6ef58332
CHARLES  AJAH <36276306+charlesajah@users.noreply.github.com>
2025-04-20 22:13:22 +0100

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,21 +1,32 @@
+println "DEBUG: Starting job definition for 'NodeJS Docker example'"
+
 job('NodeJS Docker example') {
+    println "DEBUG: Inside job block for 'NodeJS Docker example'"
+
     scm {
-        git('https://github.com/charlesajah/devops.git') {  node -> // is hudson.plugins.git.GitSCM
+        println "DEBUG: Setting up Git SCM for 'NodeJS Docker example'"
+        git('https://github.com/charlesajah/devops.git') { node ->
             node / gitConfigName('DSL User')
             node / gitConfigEmail('charles.ajah@gmail.com')
         }
     }
+
     triggers {
+        println "DEBUG: Setting SCM polling trigger"
         scm('H/5 * * * *')
     }
+
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+        println "DEBUG: Applying NodeJS wrapper"
+        nodejs('nodejs')
     }
+
     steps {
+        println "DEBUG: Starting dockerBuildAndPublish step"
         dockerBuildAndPublish {
+            println "DEBUG: Inside dockerBuildAndPublish config block"
             repositoryName('dev1980/devcharles')
-            tag('${GIT_REVISION,length=9}')
+            tag('latest')  // Temporarily simplified tag
             registryCredentials('dockerhub')
             forcePull(false)
             forceTag(false)
@@ -23,4 +34,6 @@ job('NodeJS Docker example') {
             skipDecorate()
         }
     }
+
+    println "DEBUG: Finished defining job 'NodeJS Docker example'"
 }
